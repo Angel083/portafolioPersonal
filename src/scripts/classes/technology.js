5d492f1eb9c407fa7f730c58a63946a51123e54b
AngelGM <cesargasca083@gmail.com>
2022-05-09 12:22:21 -0500

    Agrego objeto para las tecnologias

---
+++ src/scripts/classes/technology.js
@@ -0,0 +1,18 @@
+export default class Tech{
+  constructor(nombre, img){
+    this.nombre = nombre,
+    this.img = img;
+  }
+  getNombre(){
+    return this.nombre
+  }
+  getImg(){
+    return this.img
+  }
+  setNombre(nombre){
+    this.nombre = nombre
+  }
+  setImg(){
+    this.img = img
+  }
+}
