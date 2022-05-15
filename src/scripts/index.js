45119726eddfd32739d18b2197f6417f72bbd90c
AngelGM <cesargasca083@gmail.com>
2022-05-14 19:47:24 -0500

    Agrego el seguimiento de vista para hacer cambios en Navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,39 @@
+// Para el menu de navegacion
+const navbar = document.querySelector(".navbar__items")
+const indicador = document.querySelector(".indicadorInferior")
+const secciones = document.querySelectorAll(".seccion")
+
+let indicadorNav = navbar.querySelectorAll('a')
+indicador.style.width = 140+ 'px'
+
+
+const observer = new IntersectionObserver((secciones, observer) => {
+  secciones.forEach((seccion)=> {
+    if (seccion.isIntersecting) {
+      let indexSeccionActiva = seccion.target.id- 1
+      indicador.style.transform = `translateX(${140*(indexSeccionActiva )}px)`;
+      // indicador.style.transform = `translateX(${140*(indexSeccionActiva)}px)`
+      indicadorNav.forEach((index) =>{
+        index.classList.remove("active")
+        
+      })
+      indicadorNav[indexSeccionActiva].classList.add('active')
+      // console.log()
+    }
+  })
+}, {
+  rootMargin:'-100px 0px 0px 0px',
+  threshold : 0.2
+});
+
+secciones.forEach(element => observer.observe(element));
+//--- Areglar el cambio de tamaño de la pantala
+const onResize= () => {
+  navbar.querySelector('a').offsetWidth
+}
+window.addEventListener('resize', onResize)
+
+// Para las diversas tecnologias que manejo
 import Tech from "./classes/technology.js";
 import arrayTech from "./arrays/technologies.js"
 
